ecced0f3682dd4c32c571624d0a8ee5bf038b093
Jialun Chen/WHQ/Wistron <Jialun_Chen@wistron.com>
2017-07-14 10:26:09 +0800

    commit template & shell script

---
+++ azure.ps1
@@ -0,0 +1,26 @@
+﻿Get-AzureRmLocation | sort DisplayName | Select DisplayName
+New-AzureRmResourceGroup -Name "myResourceGroup" -Location " South Central US "
+New-AzureRmResourceGroupDeployment -ResourceGroupName "myResourceGroup" -TemplateUri "https://tonytrytouse.blob.core.windows.net/images/azuredeploy.json"
+
+$httprule = New-AzureRmNetworkSecurityRuleConfig `
+    -Name "myNetworkSecurityGroupRule" `
+    -Description "Allow HTTP" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority "100" `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 3000
+
+    $nsg = New-AzureRmNetworkSecurityGroup `
+    -ResourceGroupName "myResourceGroup" `
+    -Location "South Central US" `
+    -Name "myNetworkSecurityGroup" `
+    -SecurityRules $httprule
+
+$nic = Get-AzureRmNetworkInterface -ResourceGroupName myresourcegroup -Name myvmnic
+$nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName myresourcegroup  -Name mynetworksecuritygroup
+$nic.NetworkSecurityGroup = $nsg
+Set-AzureRmNetworkInterface -NetworkInterface $nic
