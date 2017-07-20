3fd494c6697602ab822e2665793962c3ee8a33e0
ChenJiaLun <jialun_chen@wistron.com>
2017-07-20 18:37:45 +0800

    Update azure.ps1

--- azure.ps1
+++ azure.ps1
@@ -1,26 +1,4 @@
-﻿Get-AzureRmLocation | sort DisplayName | Select DisplayName
+Login-AzureRmAccount
+
 New-AzureRmResourceGroup -Name "myResourceGroup" -Location " South Central US "
-New-AzureRmResourceGroupDeployment -ResourceGroupName "myResourceGroup" -TemplateUri "https://tonytrytouse.blob.core.windows.net/images/azuredeploy.json"
-
-$httprule = New-AzureRmNetworkSecurityRuleConfig `
-    -Name "myNetworkSecurityGroupRule" `
-    -Description "Allow HTTP" `
-    -Access "Allow" `
-    -Protocol "Tcp" `
-    -Direction "Inbound" `
-    -Priority "100" `
-    -SourceAddressPrefix "Internet" `
-    -SourcePortRange * `
-    -DestinationAddressPrefix * `
-    -DestinationPortRange 3000
-
-    $nsg = New-AzureRmNetworkSecurityGroup `
-    -ResourceGroupName "myResourceGroup" `
-    -Location "South Central US" `
-    -Name "myNetworkSecurityGroup" `
-    -SecurityRules $httprule
-
-$nic = Get-AzureRmNetworkInterface -ResourceGroupName myresourcegroup -Name myvmnic
-$nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName myresourcegroup  -Name mynetworksecuritygroup
-$nic.NetworkSecurityGroup = $nsg
-Set-AzureRmNetworkInterface -NetworkInterface $nic
+New-AzureRmResourceGroupDeployment -ResourceGroupName "myResourceGroup" -TemplateUri "https://github.com/ChenJiaLun/devops-on-azure/blob/master/azuredeploy.json"
