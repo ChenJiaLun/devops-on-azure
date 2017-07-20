0b34046614b6766d7b3efdbcc8192638cc901bfe
ChenJiaLun <jialun_chen@wistron.com>
2017-07-20 18:31:33 +0800

    Add files via upload

---
+++ netmon.ps1
@@ -0,0 +1,3 @@
+﻿wget https://tonytrytouse.blob.core.windows.net/images/map-7.50-setup.exe -outfile C:\map-7.50-setup.exe
+Start-Process -FilePath "C:\map-7.50-setup.exe" -ArgumentList "/S"
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\Program Files (x86)\Nmap", "Machine")
